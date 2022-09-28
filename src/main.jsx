13cf709dde285f9859c46072602a6991dc2399aa
Arnon  Ruangthanawes <arnon@gopomelo.com>
2022-09-28 21:04:29 +0700

    Router v6 tutorial

---
+++ src/main.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./error-page";
+import './index.css'
+import Contact, { loader as contactLoader, action as contactAction, } from "./routes/contact";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as deleteContact } from "./routes/destroy";
+import Index from "./routes/index";
+
+// Create with JSX
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route
+//       path="/"
+//       element={<Root />}
+//       loader={rootLoader}
+//       action={rootAction}
+//       errorElement={<ErrorPage />}
+//     >
+//       <Route errorElement={<ErrorPage />}>
+//         <Route index element={<Index />} />
+//         <Route
+//           path="contacts/:contactId"
+//           element={<Contact />}
+//           loader={contactLoader}
+//           action={contactAction}
+//         />
+//         <Route
+//           path="contacts/:contactId/edit"
+//           element={<EditContact />}
+//           loader={contactLoader}
+//           action={editAction}
+//         />
+//         <Route
+//           path="contacts/:contactId/destroy"
+//           action={destroyAction}
+//         />
+//       </Route>
+//     </Route>
+//   )
+// );
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+        action: contactAction,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: deleteContact,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
